e1729271f8db24914380b3f795b24c4361872d48
7empestx <grant.starkman@gmail.com>
2024-07-02 10:14:33 -0600

    prettier

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import { createRoot } from 'react-dom/client';
 import React from 'react';
-import App from './App'; 
+import App from './App';
 
 const rootElement = document.getElementById('root');
 
 if (rootElement) {
-  const root = createRoot(rootElement);
-  root.render(<App />);
+    const root = createRoot(rootElement);
+    root.render(<App />);
 } else {
-  console.error("Root element not found");
+    console.error('Root element not found');
 }
