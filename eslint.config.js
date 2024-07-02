2ee3a4900a4a09368262082be1fcea50b2a2541e
7empestx <grant.starkman@gmail.com>
2024-07-02 10:42:30 -0600

    lint

--- eslint.config.js
+++ eslint.config.js
@@ -2,24 +2,24 @@ const react = require('eslint-plugin-react');
 const globals = require('globals');
 
 module.exports = [
-  {
-    files: ['**/*.{js,jsx,mjs,cjs,ts,tsx}'],
-    plugins: {
-      react,
-    },
-    languageOptions: {
-      parserOptions: {
-        ecmaFeatures: {
-          jsx: true,
+    {
+        files: ['**/*.{js,jsx,mjs,cjs,ts,tsx}'],
+        plugins: {
+            react,
         },
-      },
-      globals: {
-        ...globals.browser,
-      },
+        languageOptions: {
+            parserOptions: {
+                ecmaFeatures: {
+                    jsx: true,
+                },
+            },
+            globals: {
+                ...globals.browser,
+            },
+        },
+        rules: {
+            'react/jsx-uses-react': 'error',
+            'react/jsx-uses-vars': 'error',
+        },
     },
-    rules: {
-      'react/jsx-uses-react': 'error',
-      'react/jsx-uses-vars': 'error',
-     },
-  },
 ];
